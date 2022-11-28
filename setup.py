a58f6fcff01c048f42150526400d5558f8b17874
Developer Anonymous <kibandhonor@gmail.com>
2022-11-28 19:56:50 +0100

    Agregada la instalación mediante pip git+

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    
+    name="Interactive Calculator",
+    version="1.0.0",
+    description="Una calculadora interactiva en la terminal",
+    author="Developer Anonymous#8593",
+    author_email="developeranonymous_buss@hotmail.com",
+    url="https://github.com/Developer-Anony",
+    packages=['distutils']
+
+)
