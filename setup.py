f3d8be1ce0738822384e7f21303bfa03ee55bdca
Developer Anonymous <kibandhonor@gmail.com>
2023-02-12 20:33:46 +0100

    Cambiada la descripción de PyPi > "setup.py" ha tenido un cambio de descripción que ayuda en la instalación del paquete

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     
     name="calculator_terminal",
     version="1.0.1",
-    description="Una calculadora interactiva en la terminal",
+    description="La mejor calculadora que verás, puedes utilizarla en la terminal de tu intérprete e incluso puedes utilizarla para distintas APIs que vayas a crear con sus multifunciones y todos los errores entendibles que se invocan cuando algo falla.",
     author="Developer Anonymous#8593",
     author_email="developeranonymous_buss@hotmail.com",
     url="https://github.com/Developer-Anony",
