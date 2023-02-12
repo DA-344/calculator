71391934ee61927dc4b6f0692a0553c0ae5b1b7f
Developer Anonymous <kibandhonor@gmail.com>
2023-02-12 21:10:06 +0100

    Añadidos .pypirc y mejorado setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     
     name="calculator_terminal",
-    version="1.0.1",
-    description="La mejor calculadora que verás, puedes utilizarla en la terminal de tu intérprete e incluso puedes utilizarla para distintas APIs que vayas a crear con sus multifunciones y todos los errores entendibles que se invocan cuando algo falla.",
+    version="1.0.2",
+    description="La mejor calculadora que veras, puedes utilizarla en la terminal de tu interprete e incluso puedes utilizarla para distintas APIs que vayas a crear con sus multifunciones y todos los errores entendibles que se invocan cuando algo falla.",
     author="Developer Anonymous#8593",
     author_email="developeranonymous_buss@hotmail.com",
-    url="https://github.com/Developer-Anony",
-    packages=[]
-
+    url="https://github.com/Developer-Anony/calculator",
+    packages= find_packages(),
+    license="APACHE",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apacahe License",
+        "Operating System :: OS Independent",
+    ],
 )
