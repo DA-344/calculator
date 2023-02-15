ee73accb75c37b2685741441d46e140c1f3ccb35
Developer Anonymous <kibandhonor@gmail.com>
2023-02-15 19:05:42 +0100

    Cambiados algunos archivos

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     
     name="calculator_terminal",
-    version="1.1.0",
+    version="1.1.1",
     description="La mejor calculadora que veras, puedes utilizarla en la terminal de tu interprete e incluso puedes utilizarla para distintas APIs que vayas a crear con sus multifunciones y todos los errores entendibles que se invocan cuando algo falla.",
     author="Developer Anonymous#8593",
     author_email="developeranonymous_buss@hotmail.com",
