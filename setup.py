6841017b8e31df507ac63824ac2877ab19c09352
Developer Anonymous <kibandhonor@gmail.com>
2023-05-16 20:40:59 +0200

    Nothing to say...

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name="calculator_da",
-    version="1.2.0a1",
-    description="Simple, complex-made-of calculator that has couple of things you can make with.",
-    author="Developer Anonymous#8593",
-    author_email="developeranonymous_buss@hotmail.com",
-    url="https://github.com/Developer-Anony/calculator",
-    packages=find_packages(),
-    license="APACHE",
+    name = "calculator",
+    version = "2.0.0",
+    description = "A complex-made-like calculator with multiple functions.",
+    long_description=long_description,
+    url = "https://github.com/Developer-Anony/calculator",
+    author= "Developer Anonymous",
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apacahe License",
-        "Operating System :: OS Independent",
+        "Velopment Status :: 3 - Prerelease",
+        "Topic :: Helper :: Tools",
+        "Programming Language ::Python :: 3.x"
     ],
+    keywords="tools",
+
+    packages= find_packages(where="src")
 )
